5a55be665cb3816f8361f413e951a026a7020e9f
Hoczer <123667658+Hoczer@users.noreply.github.com>
2023-01-26 20:50:02 +0100

    Add files via upload

---
+++ Koszmar Urzednika/KoszmarUrzednika/KoszmarUrzednika/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace KoszmarUrzednika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BTextBox = new System.Windows.Forms.TextBox();
+            this.CTextBox = new System.Windows.Forms.TextBox();
+            this.DTextBox = new System.Windows.Forms.TextBox();
+            this.FTextBox = new System.Windows.Forms.TextBox();
+            this.NTextBox = new System.Windows.Forms.TextBox();
+            this.MTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.WynikALabel = new System.Windows.Forms.Label();
+            this.LiczButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTextBox
+            // 
+            this.BTextBox.Location = new System.Drawing.Point(604, 35);
+            this.BTextBox.Name = "BTextBox";
+            this.BTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BTextBox.TabIndex = 0;
+            // 
+            // CTextBox
+            // 
+            this.CTextBox.Location = new System.Drawing.Point(604, 61);
+            this.CTextBox.Name = "CTextBox";
+            this.CTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CTextBox.TabIndex = 1;
+            // 
+            // DTextBox
+            // 
+            this.DTextBox.Location = new System.Drawing.Point(604, 87);
+            this.DTextBox.Name = "DTextBox";
+            this.DTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DTextBox.TabIndex = 2;
+            // 
+            // FTextBox
+            // 
+            this.FTextBox.Location = new System.Drawing.Point(604, 113);
+            this.FTextBox.Name = "FTextBox";
+            this.FTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FTextBox.TabIndex = 3;
+            // 
+            // NTextBox
+            // 
+            this.NTextBox.Location = new System.Drawing.Point(604, 139);
+            this.NTextBox.Name = "NTextBox";
+            this.NTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NTextBox.TabIndex = 4;
+            this.NTextBox.Text = "1";
+            // 
+            // MTextBox
+            // 
+            this.MTextBox.Location = new System.Drawing.Point(604, 165);
+            this.MTextBox.Name = "MTextBox";
+            this.MTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MTextBox.TabIndex = 5;
+            this.MTextBox.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(565, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "b:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(566, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "c:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(565, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "d:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(569, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "f:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(565, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "n:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label6.Location = new System.Drawing.Point(561, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "m:";
+            // 
+            // label7
+            // 
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.Location = new System.Drawing.Point(36, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(500, 185);
+            this.label7.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label8.Location = new System.Drawing.Point(35, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 24);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "WYNIK a:";
+            // 
+            // WynikALabel
+            // 
+            this.WynikALabel.AutoSize = true;
+            this.WynikALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.WynikALabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.WynikALabel.Location = new System.Drawing.Point(163, 261);
+            this.WynikALabel.Name = "WynikALabel";
+            this.WynikALabel.Size = new System.Drawing.Size(21, 24);
+            this.WynikALabel.TabIndex = 14;
+            this.WynikALabel.Text = "0";
+            // 
+            // LiczButton
+            // 
+            this.LiczButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LiczButton.Location = new System.Drawing.Point(569, 219);
+            this.LiczButton.Name = "LiczButton";
+            this.LiczButton.Size = new System.Drawing.Size(135, 34);
+            this.LiczButton.TabIndex = 15;
+            this.LiczButton.Text = "Licz";
+            this.LiczButton.UseVisualStyleBackColor = true;
+            this.LiczButton.Click += new System.EventHandler(this.LiczButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(566, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Program by B.Orzechowski.";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.LiczButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(722, 312);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LiczButton);
+            this.Controls.Add(this.WynikALabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MTextBox);
+            this.Controls.Add(this.NTextBox);
+            this.Controls.Add(this.FTextBox);
+            this.Controls.Add(this.DTextBox);
+            this.Controls.Add(this.CTextBox);
+            this.Controls.Add(this.BTextBox);
+            this.Name = "Form1";
+            this.Text = "Koszmar Urzędnika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BTextBox;
+        private System.Windows.Forms.TextBox CTextBox;
+        private System.Windows.Forms.TextBox DTextBox;
+        private System.Windows.Forms.TextBox FTextBox;
+        private System.Windows.Forms.TextBox NTextBox;
+        private System.Windows.Forms.TextBox MTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label WynikALabel;
+        private System.Windows.Forms.Button LiczButton;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Koszmar Urzednika/KoszmarUrzednika/KoszmarUrzednika/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoszmarUrzednika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LiczButton_Click(object sender, EventArgs e)
+        {
+            decimal b = 0;
+            decimal c = 0;
+            decimal d = 0;
+            decimal f = 0;
+            decimal n = 1;
+            decimal m = 1;
+            decimal a = 0;
+
+            string sb = BTextBox.Text.Replace(".", ",");
+            string sc = CTextBox.Text.Replace(".", ",");
+            string sd = DTextBox.Text.Replace(".", ",");
+            string sf = FTextBox.Text.Replace(".", ",");
+            string sn = NTextBox.Text.Replace(".", ",");
+            string sm = MTextBox.Text.Replace(".", ",");
+
+            if (Decimal.TryParse(sb, out b) && b > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'b'", "Błąd");
+                return;
+            }
+            if (Decimal.TryParse(sc, out c) && c > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'c'", "Błąd");
+                return;
+            }
+            if (Decimal.TryParse(sd, out d) && d > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'd'", "Błąd");
+                return;
+            }
+            if (Decimal.TryParse(sf, out f) && f > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'f'", "Błąd");
+                return;
+            }
+            if (Decimal.TryParse(sn, out n) && n > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'b'", "Błąd");
+                return;
+            }
+            if (Decimal.TryParse(sm, out m) && m > 0)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Błędna wartość 'm'", "Błąd");
+                return;
+            }
+
+            decimal licznik = (c * d) / f;
+            Debug.Write("licznik:"+licznik);
+            decimal mianownik=0;
+            Debug.Write("Mianownik:" + mianownik);
+            for (int i = 1; i <= m; i++)
+            {
+                Debug.Write("i:" + i);
+                decimal licznikSigma = c * i * d * i;
+                Debug.Write("licznikSigma:" + licznikSigma);
+                decimal mianownikSigma = f * i;
+                Debug.Write("mianownikSigma" + mianownikSigma);
+                mianownik += licznikSigma / mianownikSigma;
+                Debug.Write("mianownik:" + mianownik);
+
+            }
+            a = b * (licznik / mianownik);
+            WynikALabel.Text = a.ToString();
+        }
+    }
+}
